c8d66f7ed3154311894efa7278702c450f72a609
alejDev <aseckrr05@gmail.com>
2025-09-25 17:16:47 +0200

    Ejercicio 5

--- FP1EjerciciosTema2.cpp
+++ FP1EjerciciosTema2.cpp
@@ -22,8 +22,10 @@ int conversionSegundos()
 
 // Función productoOperandos()
 // Ejercicio 4.1: Desarrolla un programa que multiplique 2 numeros ENTEROS introducidos por consola y que muestre su productor por pantalla.
-int productoOperandos() {
+int productoOperandos() 
+{
     cout << "EJERCICIO 4.1 . PRODUCTO DE OPERANDOS. " << endl;
+
     int x = 0, y = 0, productoOperandos = 0;
 
     cout << "Introduce el primer operando x: ";
@@ -35,12 +37,47 @@ int productoOperandos() {
 
     cout << "El producto de los dos numeros enteros introducidos son: " << x << " x " << y << " = " << productoOperandos << endl;
     cout << "" << endl;
+
+    return 0;
+}
+
+// Funcion notaFinal()
+// Ejercicio 5: Desarrolla un programa que calcule la nota final de una asignatura de la convocatoria ordinaria a partir de:
+// nota del examen (60%), nota del proyecto (20%), actividades adicionales (20%), cada una con sus respectivos porcentajes.
+// El programa pedira al usuario sus notas, luego cualquier nota inferior a 5 no se tomara en cuenta.
+int notaFinal() 
+{
+    cout << "EJERCICIO 5. CALCULADORA DE NOTA FINAL ORDINARIA. " << endl;
+
+    const double PORCENTAJE_EXAMEN = 0.6, PORCENTAJE_PROYECTO = 0.2, PORCENTAJE_ACTIVIDADES = 0.2 ;
+    int notaExamen = 0, notaProyecto = 0, notaActividadesAdicionales = 0, notaFinal = 0;
+    
+    cout << "Introduce la nota del EXAMEN: ";
+    cin >> notaExamen ;
+    cout << "Introduce la nota del PROYECTO: ";
+    cin >> notaProyecto;
+    cout << "Introduce la nota de ACTIVIDADES ADICIONALES: " ;
+    cin >> notaActividadesAdicionales;
+
+    if (notaExamen < 5) notaExamen = 0;
+    if (notaProyecto < 5) notaProyecto = 0;
+    if (notaActividadesAdicionales < 5) notaActividadesAdicionales = 0;
+
+    notaFinal = (notaExamen * PORCENTAJE_EXAMEN) + (notaProyecto * PORCENTAJE_PROYECTO) + (notaActividadesAdicionales * PORCENTAJE_ACTIVIDADES);
+   
+    cout << "NOTAS ESTABLECIDAS POR COSOLA: " << endl 
+        << "NOTA EXAMEN: " << notaExamen << endl
+        << "NOTA PROYECTO: " << notaProyecto << endl
+        << "NOTA ACTIVIDADES: " << notaActividadesAdicionales << endl
+        << "por tanto, la NOTA FINAL es: " << notaFinal;
+    cout << "" << endl;
+
     return 0;
 }
 
 int main() {
     conversionSegundos();
     productoOperandos();
-
+    notaFinal();
     return 0;
 }
